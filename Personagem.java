c4fb6906ec6b18623b5cf101bf82019bf7aace45
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2026-07-14 00:59:28 +0000

    first commit: "Criado o construtor, o método para imprimir o status e intanciação dos personagens"

--- Personagem.java
+++ Personagem.java
@@ -1,23 +1,29 @@
+
 public class Personagem {
+    //Declaração das variáveis de instância
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
+//Construtor com as suas respectivos parâmetros
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
+//Método para imprimir as informções do personagem
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.printf("Nome: %s%n", nome);
+        System.out.printf("Classe: %s%n", classe);
+        System.out.printf("Nível: %d%n", nivel);
+        System.out.printf("Pontos de Vida: %d%n", pontosDeVida);
+        System.out.printf("Poder Base: %.2f%n%n", poderBase);
 
     }
 }
